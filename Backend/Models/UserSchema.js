c8e09e58b4a248c6b3760516a34cde2dae0010ac
Adithyan Anil <adithyananil2002@gmail.com>
2024-01-03 23:20:19 +0530

    added password reset function using crypto randombytes() method

--- Backend/Models/UserSchema.js
+++ Backend/Models/UserSchema.js
@@ -1,64 +1,79 @@
-const mongoose = require('mongoose'); // Erase if already required
-const bcrypt = require('bcrypt')
+const mongoose = require("mongoose"); // Erase if already required
+const bcrypt = require("bcrypt");
+const crypto = require("crypto");
 // Declare the Schema of the Mongo model
-var userSchema = new mongoose.Schema({
-  firstname: {
-    type: String,
-    required: true,
+var userSchema = new mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    isBlocked: {
+      type: Boolean,
+      default: false,
+    },
+    cart: {
+      type: Array,
+      default: [],
+    },
+    address: {
+      type: String,
+    },
+    wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
+    refreshToken: {
+      type: String,
+    },
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date,
   },
-  lastname: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  mobile: {
-    type: String,
-    required: false,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    default: "user",
-  },
-  isBlocked: {
-    type: Boolean,
-    default: false,
-  },
-  cart: {
-    type: Array,
-    default: [],
-  },
-  address: {
-    type: String,
-  },
-  wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
-  refreshToken: {
-    type: String,
-  },
-  passwordChangedAt: Date,
-  passwordResetToken: String,
-  passwordResetExpires: Date,
-},
   {
     timestamps: true,
   }
 );
 
-userSchema.pre('save', async function (next) {
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
   const salt = await bcrypt.genSaltSync(10);
-  this.password = await bcrypt.hash(this.password, salt)
-})
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 userSchema.methods.isPassMatched = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password)
-}
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+userSchema.methods.createPasswordResetToken = async function () {
+  const resettoken = crypto.randomBytes(32).toString("hex");
+  this.passwordResetToken = crypto
+    .createHash("sha256")
+    .update(resettoken)
+    .digest("hex");
+  this.passwordResetExpires = Date.now() + 30 * 60 * 1000; // 10 minutes
+  return resettoken;
+};
 
 //Export the model
-module.exports = mongoose.model('users', userSchema);
+module.exports = mongoose.model("users", userSchema);
